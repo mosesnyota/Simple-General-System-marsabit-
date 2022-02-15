f0df4c1e3a7be47bb0ed52d8c14db98572a7c178
Moses Nyota <mosesnyota@gmail.com>
2022-02-15 09:26:50 +0300

    Updated send sms for lazy loading

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -30,71 +30,103 @@ class SmsController extends Controller
         $input = $request->all();
         $target = $input['targetgroup'];
         $mymessage = $input['messages'];
-        //<option value="students">All Students</option>
-        //<option value="staff">All Staff</option>
-        //<option value="alumni">Alumni</option>
-        //<option value="customers">Registered Customers</option>
-        //<option value="all">Every Body</option>
-
         $sqlQuery = "";
-
         if($target == 'students'){
             $sqlQuery = "SELECT `student_id`,`student_no`,`first_name`,`middle_name`,`surname`,`phone`, cur_status, CHAR_LENGTH(phone) FROM `students`
             WHERE `deleted_at` IS NULL AND cur_status = 'Active' AND phone IS NOT NULL AND phone != '0' 
-            AND (CHAR_LENGTH(phone) > 8 AND CHAR_LENGTH(phone) < 13 AND phone REGEXP '[0-9]' ) and student_id =404 ";
-        }else if($target == 'staff'){
-            $sqlQuery = "";
+            AND (CHAR_LENGTH(phone) > 8 AND CHAR_LENGTH(phone) < 13 AND phone REGEXP '[0-9]' )  ";
+    
+            $toSendTo =  DB::select( DB::raw($sqlQuery));
+            foreach($toSendTo as $cst){ 
+                $phone = $cst ->phone;
+                $name = $cst ->first_name." ".$cst ->middle_name." ".$cst ->surname;
+                
+                $this->sendSMS2($phone,$mymessage,$target, $name );
+
+            }//end foreach
+
+
+    }else if($target == 'staff'){
+            $sqlQuery = "SELECT `firstname`,`othernames`, `phone` FROM `staff`
+            WHERE `deleted_at` IS NULL AND phone IS NOT NULL AND phone != '0' 
+            AND (CHAR_LENGTH(phone) > 8 AND CHAR_LENGTH(phone) < 13 AND phone REGEXP '[0-9]' )";
+            $toSendTo =  DB::select( DB::raw($sqlQuery));
+            foreach($toSendTo as $cst){ 
+                $phone = $cst ->phone;
+                $name = $cst ->firstname." ".$cst ->othernames;
+                
+                $this->sendSMS2($phone,$mymessage,$target, $name );
+
+            }//end foreach
         }else if($target == 'alumni'){
-            $sqlQuery = "";
+            $sqlQuery = "SELECT `student_id`,`student_no`,`first_name`,`middle_name`,`surname`,`phone`, cur_status, CHAR_LENGTH(phone) FROM `students`
+            WHERE `deleted_at` IS NULL AND cur_status != 'Active' AND phone IS NOT NULL AND phone != '0' 
+            AND (CHAR_LENGTH(phone) > 8 AND CHAR_LENGTH(phone) < 13 AND phone REGEXP '[0-9]' )  ";
+            $toSendTo =  DB::select( DB::raw($sqlQuery));
+            foreach($toSendTo as $cst){ 
+                $phone = $cst ->phone;
+                $name = $cst ->first_name." ".$cst ->middle_name." ".$cst ->surname;
+                
+                $this->sendSMS2($phone,$mymessage,$target, $name );
+
+            }//end foreach
         }else if($target == 'customers'){
-            $sqlQuery = "";
+            $sqlQuery = "SELECT `customer_names`,`phone` FROM `customers`
+            WHERE `deleted_at` IS NULL AND phone IS NOT NULL AND phone != '0' 
+            AND (CHAR_LENGTH(phone) > 8 AND CHAR_LENGTH(phone) < 13 AND phone REGEXP '[0-9]' )";
+            $toSendTo =  DB::select( DB::raw($sqlQuery));
+            foreach($toSendTo as $cst){ 
+                $phone = $cst ->phone;
+                $name = $cst ->customer_names;
+                
+                $this->sendSMS2($phone,$mymessage,$target, $name );
+
+            }//end foreach
         }
 
-        $toSendTo =  DB::select( DB::raw($sqlQuery));
-        foreach($toSendTo as $cst){ 
-            $phone = $cst ->phone;
-            $name = $cst ->first_name." ".$cst ->middle_name." ".$cst ->surname;
-            //this eliminates phones with characters or special characters
-            if (preg_match('/[A-Za-z]/', $phone) || preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $phone) )
-                { 
-                    //Data cleaning to eliminate phone numbers with characters
-                    //echo   $phone . '    Contains at least one letter and one number'. "<br>";
-                }  else{
-                   
-                
-                $msisdn = $phone;
-                $Message = $mymessage;
-                $messages = urlencode(stripslashes($Message)); // the message to the recepients	
-                $ch = curl_init();
-                $myurl = "https://client.airtouch.co.ke:9012/sms/api/?issn=DONBOSCOMBT&msisdn=$msisdn&text=$messages&username=donbosco&password=b28630c1a54cce60c3b89d4e72909387";
-                curl_setopt($ch, CURLOPT_URL,  $myurl);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $output = curl_exec($ch);
-                curl_close($ch);
-                $json = json_decode($output);
-                //var_dump(json_decode($output));
-                $statuscode =  $json->status_code;
-                $statusdesc =  $json->status_desc;
-                
-                $sentmsgslog = Array();
-                $sentmsgslog['phone'] =  $phone ;
-                $sentmsgslog['statuscode'] = $statuscode  ;
-                $sentmsgslog['statusdesc'] =   $statusdesc;
-                $sentmsgslog['messages'] =  $mymessage ;
-                $sentmsgslog['targetgroup'] =  $target  ;
-                $sentmsgslog['name'] =   $name  ;
-                SentSMSLog::create($sentmsgslog);
-
-
-            
- 
-            }//end else... 
-
-        }//end foreach
+        
 
         return redirect()->action(
             'SmsController@index'
         );
+
+        
+    }
+
+
+    public function sendSMS2($phone,$mymessage,$target, $name ){
+         //this eliminates phones with characters or special characters
+         if (preg_match('/[A-Za-z]/', $phone) || preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $phone) )
+            { 
+                //Data cleaning to eliminate phone numbers with characters
+                //echo   $phone . '    Contains at least one letter and one number'. "<br>";
+            }  else{
+               
+            
+            $msisdn = $phone;
+            $Message = $mymessage;
+            $messages = urlencode(stripslashes($Message)); // the message to the recepients	
+            $ch = curl_init();
+            $myurl = "https://client.airtouch.co.ke:9012/sms/api/?issn=DONBOSCOMBT&msisdn=$msisdn&text=$messages&username=donbosco&password=b28630c1a54cce60c3b89d4e72909387";
+            curl_setopt($ch, CURLOPT_URL,  $myurl);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $output = curl_exec($ch);
+            curl_close($ch);
+            $json = json_decode($output);
+            //var_dump(json_decode($output));
+            $statuscode =  $json->status_code;
+            $statusdesc =  $json->status_desc;
+            
+            $sentmsgslog = Array();
+            $sentmsgslog['phone'] =  $phone ;
+            $sentmsgslog['statuscode'] = $statuscode  ;
+            $sentmsgslog['statusdesc'] =   $statusdesc;
+            $sentmsgslog['messages'] =  $mymessage ;
+            $sentmsgslog['targetgroup'] =  $target  ;
+            $sentmsgslog['name'] =   $name  ;
+            SentSMSLog::create($sentmsgslog);
+
+        }//end else...
 
         
     }
